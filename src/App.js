462697c1ce1e7e2fae4656939cce55b8ad3d6cc5
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-08-01 18:02:45 -0300

    menu bar and other minor fixes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Eunoia Mia  
-    </div>
-  );
+  return " ";
 }
 
 export default App;
