b8658f0f51a99abb2dd959c77a556b7eb76adcd3
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-08-01 19:46:02 -0300

    a lot of things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import MenuBar from './components/MenuBar'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      Eunoia Mia  
+    <div>
+      <Header />
+      <MenuBar />
     </div>
   );
 }
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const MenuBar = () => {
+    return (
+    <div class="menu-bar">
+      <button class="menu-btn">Inicio</button>
+      <img src="separator.png" height="30px"/>
+      <div class="dropdown">
+        <button class="dropbtn">Productos</button>
+        <div class="dropdown-content">
+          <a href="#">Cuidado capilar</a>
+          <a href="#">Salud bucal</a>
+          <a href="#">Aromaterapia</a>
+          <a href="#">Desodorante natural</a>
+          <a href="#">Cuidado de la piel</a>
+          <a href="#">Jabones vegetales</a>
+          <a href="#">Accesorios sustentables</a>
+          <a href="#">Almohadillas termicas</a>
+        </div>
+      </div>
+      <img src="separator.png" height="30px" />
+      <button class="menu-btn">Info</button>
+      <img src="separator.png" height="30px" />
+      <button class="menu-btn">Quienes somos</button>
+      <img src="separator.png" height="30px" />
+      <button class="menu-btn">Contacto</button>
+      <div class="search-container">
+        <form action="">
+          <input type="search" />
+          <button>
+            <img height="20px" src="search.ico"/>
+          </button>
+        </form>
+      </div>
+      <img class="search-container" height="40px" src="shop.ico"/>
+          
+    </div>
+  );
+};
+
+export default MenuBar;
