49325912b19bb52d656546c590c408289ea1cb9c
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-08-10 18:58:03 -0300

    hamburguer

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,47 +3,41 @@ import {Link} from 'react-router-dom'
 
 const MenuBar = () => {
     return (
-    <div class="menu-bar">
-      <button class="menu-btn">
-        <Link to = "/" className = "link">Inicio</Link>
-      </button>
-      <img alt="separator" src="/separator.png" height="30px"/>
-      <div class="dropdown">
-        <button class="dropbtn">
-         <Link to = "/products" className = "link">Productos</Link>
-        </button>        
-        <div class="dropdown-content">
-          <button className="menu-btn">Combos</button>
-          <button className="menu-btn">Cuidado capilar</button>
-          <button className="menu-btn">Cuidado de la piel</button>
-          <button className="menu-btn">Salud bucal</button>
-          <button className="menu-btn">Desodorante natural</button>
-          <button className="menu-btn">Jabones vegetales</button>
-          <button className="menu-btn">Accesorios sustentables</button>
-          <button className="menu-btn">Almohadillas térmicas</button>
-          <button className="menu-btn">Aromaterapia</button>
-        </div>
-      </div>
-      <img alt="separator" src="/separator.png" height="30px" />
-      <button class="menu-btn">
-        <Link to = "/info" className = "link">Info</Link>
-      </button>
-      <img alt="separator" src="/separator.png" height="30px" />
-      <button class="menu-btn">
-        <Link to = "/whoarewe" className = "link">Quiénes somos</Link>
-      </button><img alt="separator" src="/separator.png" height="30px" />
-      <button class="menu-btn">
-        <Link to = "/contact" className = "link">Contacto</Link>
-      </button><div class="search-container">
-        <form action="">
-          <input type="search" />
-          <button>
-            <img alt="Buscar"height="20px" src="/search.ico"/>
-          </button>
-        </form>
-      </div>
-      <img class="shop" alt="shop" height="60px" src="/shop.ico"/>
-          
+      <div>
+
+      
+    <nav className="menu-bar">
+      <input type="checkbox" id="check"/>
+      <label for="check" class="checkbtn">
+        <img src="hamburguer.png" height="30px"></img>
+      </label>
+      <ul>
+        <li><Link to = "/" className = "link">Inicio</Link></li>
+        <img alt="separator" src="/separator.png" height="30px" />
+        <li>
+          <div className="dropdown">
+            <Link to = "/products" className = "dropbtn">Productos</Link>
+            <div class="dropdown-content">
+              <button className="menu-btn">Combos</button>
+              <button className="menu-btn">Cuidado capilar</button>
+              <button className="menu-btn">Cuidado de la piel</button>
+              <button className="menu-btn">Salud bucal</button>
+              <button className="menu-btn">Desodorante natural</button>
+              <button className="menu-btn">Jabones vegetales</button>
+              <button className="menu-btn">Accesorios sustentables</button>
+              <button className="menu-btn">Almohadillas térmicas</button>
+              <button className="menu-btn">Aromaterapia</button>
+            </div>
+          </div>
+        </li>
+        <img alt="separator" src="/separator.png" height="30px" />
+        <li><Link to = "/info" className = "link">Info</Link></li>
+        <img alt="separator" src="/separator.png" height="30px" />
+        <li><Link to = "/whoweare" className = "link">Quiénes somos</Link></li>
+        <img alt="separator" src="/separator.png" height="30px" />
+        <li><Link to = "/contact" className = "link">Contacto</Link></li>
+      </ul> 
+    </nav>
     </div>
   );
 };
