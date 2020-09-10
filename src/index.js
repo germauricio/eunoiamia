3a2c97924e901f8596921b8f1d6dcea094584c5a
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-09-10 01:59:54 -0300

    no se

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {CartProvider} from './services/cartContext'
-
-ReactDOM.render(<CartProvider> <App /> </CartProvider>, document.getElementById('root'));
