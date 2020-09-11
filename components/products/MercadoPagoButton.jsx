8a15dff1326650eecb9538c1c96ebce98e33e4ab
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-09-11 03:42:02 -0300

    ahora si

--- components/products/MercadoPagoButton.jsx
+++ components/products/MercadoPagoButton.jsx
@@ -4,11 +4,10 @@ import { generatePreference } from '../../services/mercadoPago';
 export default function MercadoPagoButton ({ price, name, quantity }) {
   const setMercadoPagoPreferences = async () => {
     const initPoint = await generatePreference(price, name);
-    console.log(initPoint)
     const script = document.createElement('script');
     script.src = 'https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js';
     script.async = true;
-    script.setAttribute('data-preference-id', initPoint.preferenceId);
+    script.setAttribute('data-preference-id', initPoint);
     document.getElementById('mercadoForm').appendChild(script);
   };
   
