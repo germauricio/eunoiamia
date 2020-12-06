d2e144df6e8e40b2ea222c375d30ea8d56baeeb5
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-12-06 18:41:28 -0300

    increase stock on sell

--- services/apiService.js
+++ services/apiService.js
@@ -14,6 +14,14 @@ export const getProductsByCategory = async (category) => {
     })).data;
 }
 
+export const editProduct = async (params) => {
+
+    return (await axios.put(url + '/api/product', params, 
+    {
+        headers: {"Access-Control-Allow-Headers": "*"}
+    })).data;
+  }
+
 export const getProduct = async (productName) => {
     return (await axios.get(url + '/api/product/' + productName, 
     {
@@ -25,6 +33,8 @@ export const makeSell = async (cart, totalPrice, user, shipment) => {
     var sellProductsName = '';
 
     cart.map( product => {
+        product.stock =- 1
+        await editProduct(product);
         sellProductsName += `${product.description} x${product.quantity} `
     });
 
