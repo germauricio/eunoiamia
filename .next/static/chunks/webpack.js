e8251e6552573b8f0b2bc5923f465a6a47846001
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-12-06 17:28:01 -0300

    filter

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3e1d4da5ab23e5c33402";
+/******/ 	var hotCurrentHash = "ad714f7e10834c281da9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/products.jsx
+++ pages/products.jsx
@@ -4,21 +4,49 @@ import ProductList from '../components/products/ProductList';
 
 function Products(){
     const [products, setProducts] = useState(false);
+    const [filter, setFilter] = useState('');
+    const [filtrableProducts, setFiltrableProducts] = useState(false);
+
+    const handleFilter = (e) => {
+        e.preventDefault();
+        const filteredProducts = filtrableProducts.filter(item => item.description.toLowerCase().includes(filter))
+
+        setProducts(filteredProducts);
+    }
 
     useEffect( () => {
         (async () => {
             let gettedProducts = await getProducts();
-            setProducts(gettedProducts);       
+            setProducts(gettedProducts);
+            setFiltrableProducts(gettedProducts);
         })()
     }, []);
 
     return(
         <div className="container">
-        { products ? (
-            <ProductList products={products} />
-        ) : (
-            <img src="/Rolling-1s-200px.gif" className="loading" alt="loading"/>
-        )}
+            <div className="col-sm-4 center">
+                <form 
+                class="input-group"
+                onSubmit={handleFilter}
+                >
+                    <input 
+                    type="text" 
+                    class="form-control"
+                    placeholder="¡Sumate al cambio!"
+                    onChange={e => setFilter(e.target.value)}/>
+                    <div class="input-group-append mb-5"><button class="btn btn-primary">Buscar</button></div>
+                </form>
+            </div>
+            <div className="container">
+            { products ? (
+                <div>
+                    
+                    <ProductList products={products} />
+                </div>
+            ) : (
+                <img src="/Rolling-1s-200px.gif" className="loading" alt="loading"/>
+                )}
+            </div>
         </div>
     )
 }
